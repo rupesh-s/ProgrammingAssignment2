e9f7693ebc560e403faaa404f4cb7b616f87cd55
rupesh-s <rupeshsmurty@yahoo.co.in>
2015-09-28 01:11:03 +0530

    olution to Assignment 2 by Rupesh Shantamurty

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file implements two functions as stated below
+## makeCacheMatrix - Generates a "matrix object that can cache it's inverse
+## cacheSolve - Computes the inverse of a matrix
+## NOTE : Assuming that the matrix supplied is always invertible
 
-## Write a short comment describing this function
-
+## Function to create a matrix to cache it's  inverse
+## input data type: matrix
+## return data type : matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## initialize a cache to store the inverse of the matrix
+  localCache <- NULL
+  ## Setter function to create a matrix
+  set <- function(y) {
+    x <<- y
+    localCache <<- NULL
+  }
+  ## getter function for the input matrix
+  get <- function() x
+  ## setter function for assigning values to the inverse of matrix 
+  setInverse <- function(inversematrix) localCache <<- inversematrix
+  ## getter function to retreve the value of the inverse of matrix
+  getInverse <- function() localCache
+  
+  ## create a list of functions exposed
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Function to generate the inverse of a matrix 
+## input data type : matrix
+## return data type : matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversematrix <- x$getInverse()
+  
+  ## check if the inverse already exists
+  if(!is.null(inversematrix)){
+    return(inversematrix)
+  }
+  ## if not then generate inverse and "set" it
+  inputdata <- x$get()
+  inversematrix <- solve(inputdata, ...)
+  x$setInverse(inversematrix)
+  ## Return a matrix that is the inverse of 'x'
+  inversematrix
 }
